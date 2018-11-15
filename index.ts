9987c1b9c79e3f4f42bf000d2964c0e2b1e484bc
Yan Chen <yanche@microsoft.com>
2018-11-14 16:56:53 -0800

    from json util

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 
 import { deserialize } from "./deserialize";
 
-export function fromObject<T>(json: { [key: string]: any }, ctor: new () => T): T {
-    return deserialize<T>(json, ctor);
+export function fromObject<T>(obj: { [key: string]: any }, ctor: new () => T): T {
+    return deserialize<T>(obj, ctor);
+}
+
+export function fromJson<T>(json: string, ctor: new () => T): T {
+    let objFromJson: any = null;
+    try {
+        objFromJson = JSON.parse(json);
+    }
+    catch (err) {
+        throw new Error("input json is invalid");
+    }
+    return fromObject<T>(objFromJson, ctor);
 }
